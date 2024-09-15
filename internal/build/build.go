7340663e5d0b365a5612cbf772a275ce23f18d48
sk1m <purrtoujour@gmail.com>
2024-09-15 12:13:36 +0300

    goreleaser: use Tag instead of Version in the version setting

--- internal/build/build.go
+++ internal/build/build.go
@@ -1,20 +1,10 @@
 package build
 
-import "runtime/debug"
-
 // Version is dynamically set by the toolchain or overridden by the Makefile.
 var version = "DEV"
 
 // Date is dynamically set at build time in the Makefile.
 var date = "" // YYYY-MM-DD
-
-func init() {
-	if version == "DEV" {
-		if info, ok := debug.ReadBuildInfo(); ok && info.Main.Version != "(devel)" {
-			version = info.Main.Version
-		}
-	}
-}
 
 func Version() string {
 	return version
